e225b9bb14e6c3cfd1764347f09a2946d468c946
chakrabortyanish <chakrabortyanish981@gmail.com>
2024-10-27 16:15:12 +0530

    Add new feature

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const btn = document.querySelectorAll(".box");
 const msg = document.getElementById("msg");
 const resetGame = document.getElementById("reset");
+const sound = new Audio("image/bubblepop-254773.mp3");    // button click sound
+const winningSong = new Audio("image/winning-song.wav");   // winning song
+const game_over = new Audio("image/game-over.mp3");    //  game over sound 
 
 let winPattern = [  // All win patterns
     [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
     [3, 4, 5],
     [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
     [2, 4, 6]
 ]
 
@@ -19,8 +22,19 @@ function disableBtn(value){  // when will we click on the box then the box will 
 
 let playerX = true;
 
+function playerTurn(){  //player turn function
+    if(playerX){
+        msg.innerHTML = `Player X turn`;
+    }
+    else{
+        msg.innerHTML = "Player O turn";
+    }
+}
+playerTurn();
+
 btn.forEach((item)=>{
     item.addEventListener("click",()=>{
+        sound.play();
         if(playerX){
             item.innerHTML = "X";
             item.classList.add("playerX");
@@ -30,41 +44,88 @@ btn.forEach((item)=>{
             item.innerHTML = "O";
             item.classList.add("playerO");
             playerX = true;
-        }
-        disableBtn(item);  // Called the disablebtn function
+        }  
+        playerTurn();
+        disableBtn(item);   // Called the disablebtn function
         checkItem(item);
-    })
-})
+    });
+});
 
 function showWinner(win){
-    msg.innerHTML = `Winner is:  ${win}`;
+    winningSong.play();
+    msg.innerHTML = `${win}  Won`;
     for(let i=0; i<btn.length; i++){  // After wining all boxs display will be disabled
         disableBtn(btn[i]);
     }
 }
 
-function checkItem(){
-   for(pattern of winPattern){
-     
-    let fristval = btn[pattern[0]].innerText;
-    let secoundval = btn[pattern[1]].innerText;
-    let thirdval = btn[pattern[2]].innerText;
-    
-    if(fristval != "" && secoundval != "" && thirdval != ""){
-        if(fristval == secoundval && secoundval == thirdval){
-            showWinner(fristval);
-        }
+function matchtie(){
+    if(btn[0].innerText !== "" && btn[1].innerText !== "" && btn[2].innerText !== "" && 
+        btn[3].innerText !== "" && btn[4].innerText !== "" && btn[5].innerText !== "" && 
+        btn[6].innerText !== "" && btn[7].innerText !== "" && btn[8].innerText !== ""){
+        game_over.play();
+        msg.innerHTML = `Result is:  Draw`;
     }
+}
+
+function checkItem(){   
+   for(pattern of winPattern){
+        
+        if(btn[pattern[0]].innerText !== "" && btn[pattern[1]].innerText !== "" && btn[pattern[2]].innerText !== ""){
+            
+            if(btn[pattern[0]].innerText === btn[pattern[1]].innerText && btn[pattern[1]].innerText === btn[pattern[2]].innerText){
+                showWinner(btn[pattern[0]].innerText);
+                btn[pattern[0]].classList.add("won-color");
+                btn[pattern[1]].classList.add("won-color");
+                btn[pattern[2]].classList.add("won-color");
+                displayWinningLine(btn[pattern[0]].id, btn[pattern[1]].id, btn[pattern[2]].id);
+            }
+            else if(btn[pattern[0]].innerText !== btn[pattern[1]].innerText && btn[pattern[1]].innerText !== btn[pattern[2]].innerText){
+                matchtie();
+            }
+        }
+
    }
 }
 
+function displayWinningLine(val1, val2, val3){   // winning line show function
+    if(btn[0].id === val1 && btn[1].id === val2 && btn[2].id === val3){
+       document.getElementById("r1").style.display = "block";
+    }
+    else if(btn[3].id === val1 && btn[4].id === val2 && btn[5].id === val3){
+        document.getElementById("r2").style.display = "block";
+    }
+    else if(btn[6].id === val1 && btn[7].id === val2 && btn[8].id === val3){
+        document.getElementById("r3").style.display = "block";
+    }
+    else if(btn[0].id === val1 && btn[3].id === val2 && btn[6].id === val3){
+        document.getElementById("c1").style.display = "block";
+    }
+    else if(btn[1].id === val1 && btn[4].id === val2 && btn[7].id === val3){
+         document.getElementById("c2").style.display = "block";
+    }
+    else if(btn[2].id === val1 && btn[5].id === val2 && btn[8].id === val3){
+         document.getElementById("c3").style.display = "block";
+    }
+    else if(btn[0].id === val1 && btn[4].id === val2 && btn[8].id === val3){
+        document.getElementById("x_left").style.display = "block";
+    }
+    else if(btn[2].id === val1 && btn[4].id === val2 && btn[6].id === val3){
+         document.getElementById("x_right").style.display = "block";
+    }
+}
 
-resetGame.addEventListener("click",()=>{
+resetGame.addEventListener("click",()=>{  // reset game
     for(let button of btn){
-        msg.innerHTML = "";
+        playerTurn();
         button.innerHTML = "";
         button.disabled = false;
         button.classList.remove("playerO");
         button.classList.remove("playerX");
+        button.classList.remove("won-color");
     }
-})
+
+    document.querySelectorAll(".win_line").forEach((line) =>{
+        line.style.display = "none";   // winning line hidden
+    });
+});
